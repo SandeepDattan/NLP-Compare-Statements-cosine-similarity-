5cc4d015ec755b4c9b7e441f634cdd0a501a2025
Sandeep <dattan.sandeep@gmail.com>
2022-11-22 13:38:33 +0530

    update

--- Addition.py
+++
@@ -1,7 +0,0 @@
-
-num1 = 2.3
-num2 = 3.7
-
-num3 = num1 + num2
-
-print(num3)
--- Deployment/test.py
+++
@@ -1,6 +0,0 @@
-print('hello world')
-
-a = 'hello world'
-for index,value in enumerate(a):
-    if value == 'o':
-        print(index)
